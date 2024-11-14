e043b78793f01ae717e719d59cd216a2220193af
Timothy Chang <tyc2118@columbia.edu>
2024-11-14 18:47:30 -0500

    start customer process

--- sim.py
+++ sim.py
@@ -1,4 +1,3 @@
-import time
 from numpy import random as rng
 import simpy
 
@@ -13,8 +12,9 @@ class System:
         self.servers = simpy.Resource(env, capacity=self.num_servers)
 
         self.max_customer_cnt = max_customer_cnt
+        env.process(self.customers(env))
 
-    def customer(self, env):
+    def customers(self, env):
         for _ in range(self.max_customer_cnt):
             env.process(self.request(env))
             yield self.env.timeout(rng.exponential(scale=self.arrival_time))
@@ -25,10 +25,13 @@ class System:
             yield env.timeout(rng.exponential(scale=self.service_time))
 
 if __name__ == "__main__":
+    print("setup variables.")
     arrival_rate = 1
     service_rate = 1
     max_customer_cnt = 10000
     env = simpy.Environment()
 
+    print("initialize sim")
     system = System(env, arrival_rate, service_rate, max_customer_cnt)
     env.run()
+    print("done.")
