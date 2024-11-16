f42eec8cae304aa10aec3cd8cd74e2f69ce8c212
Timothy Chang <tyc2118@columbia.edu>
2024-11-16 14:58:44 -0500

    cmd_line arg, tracing, print values

--- sim.py
+++ sim.py
@@ -1,35 +1,103 @@
 import sys
 from numpy import random as rng
 import simpy
+from functools import partial, wraps
+import pandas as pd
 
 class System:
     def __init__(self, env, arrival_rate, service_rate, max_customer_cnt, num_servers):
         self.env = env
         self.arrival_time = 1/arrival_rate
         self.service_time = 1/service_rate
+        self.max_customer_cnt = max_customer_cnt
         self.num_servers = num_servers
 
-        self.server_list = []
+        self.cid_data = []
+
         self.servers = simpy.Resource(env, capacity=self.num_servers)
 
-        self.max_customer_cnt = max_customer_cnt
         env.process(self.customers(env))
 
     def customers(self, env):
+        cid = 0
         for _ in range(self.max_customer_cnt):
-            env.process(self.request(env))
+            env.process(self.request(env, cid))
             yield self.env.timeout(rng.exponential(scale=self.arrival_time))
+            cid += 1
 
-    def request(self, env):
+    def request(self, env, cid):
+        self.cid_data.append((env.now, cid))
         with self.servers.request() as req:
             yield req
             yield env.timeout(rng.exponential(scale=self.service_time))
+            self.cid_data.append((env.now, cid))
+
+def patch_resource(resource, pre=None, post=None):
+    """Patch *resource* so that it calls the callable *pre* before each
+    put/get/request/release operation and the callable *post* after each
+    operation.  The only argument to these functions is the resource
+    instance.
+    code from simpy documentation.
+    """
+
+    def get_wrapper(func):
+        # Generate a wrapper for put/get/request/release
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            # This is the actual wrapper
+            # Call "pre" callback
+            if pre:
+                pre(resource)
+
+            # Perform actual operation
+            ret = func(*args, **kwargs)
+
+            # Call "post" callback
+            if post:
+                post(resource)
+
+            return ret
+        return wrapper
+
+
+    # Replace the original operations with our wrapper
+    for name in ['request', 'release']:
+        if hasattr(resource, name):
+            setattr(resource, name, get_wrapper(getattr(resource, name)))
+
+def monitor(data, resource):
+    """This is our monitoring callback.
+    code from simpy documentation.
+    """
+    item = (
+        resource._env.now,  # The current simulation time
+        resource.count,  # The number of users
+        len(resource.queue),  # The number of queued processes
+    )
+    data.append(item)
+
+def analyze_data(cid_data, resource_data):
+    avg_q_len, avg_resp_time, server_utilization = 0, 0, 0
+
+    cid_df = pd.DataFrame(cid_data, columns = ['time', 'cid'])
+    avg_resp_time = cid_df.groupby(['cid']).diff().mean()['time']
+
+    resource_df = pd.DataFrame(resource_data, columns = ['time', 'users', 'q_len'])
+    resource_df['time_diff'] = resource_df['time'].shift(-1) - resource_df['time']
+    resource_df.dropna(inplace=True)
+    resource_df['q_len_weight'] = resource_df['q_len'] * resource_df['time_diff']
+    resource_df['util_weight'] = resource_df['users'] * resource_df['time_diff']
+    server_utilization = resource_df.mean()['util_weight']
+    avg_q_len = resource_df.mean()['q_len_weight']
+    #print(resource_df.head(10)) 
+
+    return (avg_q_len, avg_resp_time, server_utilization)
 
 if __name__ == "__main__":
     assert len(sys.argv) == 3 or len(sys.argv) == 4, f"invalid parameter count: {len(sys.argv)}"
-    arrival_rate = int(sys.argv[1])
+    arrival_rate = float(sys.argv[1])
     print(f"arrival_rate: {arrival_rate}")
-    service_rate = int(sys.argv[2])
+    service_rate = float(sys.argv[2])
     print(f"service_rate: {service_rate}")
     if (len(sys.argv) == 4):
         num_servers = int(sys.argv[3])
@@ -38,10 +106,18 @@ if __name__ == "__main__":
     print(f"num_servers: {num_servers}")
     print("setup variables done.")
 
+    resource_data = []
     max_customer_cnt = 10000
     env = simpy.Environment()
 
     print("initialize sim")
     system = System(env, arrival_rate, service_rate, max_customer_cnt, num_servers)
+    monitor = partial(monitor, resource_data)
+    patch_resource(system.servers, post=monitor)
     env.run()
     print("sim done.")
+
+    avg_q_len, avg_resp_time, server_utilization = analyze_data(system.cid_data, resource_data)
+    print(f"avg_q_len: {avg_q_len}")
+    print(f"avg_resp_time: {avg_resp_time}")
+    print(f"server_utilization: {server_utilization}")
